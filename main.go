b2e0e0f87e9f52fd8ab729ef222cdc6296d4cc8b
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-02-21 21:59:29 +0000

    :sparkles: feat: Add a bubble sort function with number generic type

--- main.go
+++ main.go
@@ -7,6 +7,19 @@ type Number interface {
 		float32 | float64
 }
 
+func BubbleSort[N Number](input []N) []N {
+	n := len(input)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if input[j] > input[j+1] {
+				input[j], input[j+1] = input[j+1], input[j]
+			}
+		}
+	}
+
+	return input
+}
+
 func newGenericFunc[age Number](myage age) {
 	val := int(myage) + 1
 	fmt.Println(val)
@@ -14,6 +27,11 @@ func newGenericFunc[age Number](myage age) {
 
 func main() {
 	fmt.Println("Go Generics!!")
+
+	listints := []int32{4, 1, 13, 54, 13, 6}
+	listfloats := []float32{.4, .1, 1.3, 5.4, 13.2, .06}
+	fmt.Println(BubbleSort(listints))
+	fmt.Println(BubbleSort(listfloats))
 
 	var age int64 = 26
 	var age2 float32 = 26.6
