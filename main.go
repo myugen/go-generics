a768bd76b78269793c8f72940dda369d6a050e62
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-02-21 21:39:52 +0000

    :sparkles: feat: Add first generic function

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+func newGenericFunc[age int64 | float32](myage age) {
+	fmt.Println(myage)
+}
+
 func main() {
 	fmt.Println("Go Generics!!")
+
+	var age int64 = 26
+	var age2 float32 = 26.6
+
+	newGenericFunc(age)
+	newGenericFunc(age2)
 }
